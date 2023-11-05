6ef6e43825873a8c89987d27f4d0a9401c0de102
pnanstasia <kucheruk.pn@ucu.edu.ua>
2023-11-05 02:07:56 +0200

    update

--- src/main/java/ua/edu/ucu/lab/flowers/FlowerRepository.java
+++ src/main/java/ua/edu/ucu/lab/flowers/FlowerRepository.java
@@ -1,7 +1,9 @@
 package ua.edu.ucu.lab.flowers;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface FlowerRepository extends JpaRepository<Flower, Integer> {
     
 }
